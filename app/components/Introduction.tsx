ba93b0a208bcbee63a24dce66ffc483312d9a4d7
arhantsg07 <sfarhant098@gmail.com>
2026-03-04 00:14:32 +0530

    responsiveness related changes

--- app/components/Introduction.tsx
+++ app/components/Introduction.tsx
@@ -8,10 +8,10 @@ const Introduction: React.FC = () => {
         <>
             <div className="flex flex-col text-lg text-center justify-center items-center font-serif space-y-5 mt-15">
                 <Map />
-                <div className={`border-2 border-[#383838] font-['IBMPlexSans'] text-lg rounded-lg p-5 mb-20 ${themeClasses.textHolder} ${themeClasses.skillText}`}>
+                <div className={`border-2 border-[#383838] font-['IBMPlexSans'] text-lg rounded-lg p-5 mb-20 mx-5 ${themeClasses.textHolder} ${themeClasses.skillText}`}>
                     Hello, I am a curious programmer and developer !
                 </div>
-                <div className={`${themeClasses.text} font-['IBMPlexSans'] text-justify max-w-[50ch] break-words`}>
+                <div className={`${themeClasses.text} font-['IBMPlexSans'] text-justify max-w-[50ch] break-words px-5 md:px-10`}>
                     Hi everyone, I am Arhant, currently a student of Computer Science and Engineering at IIIT kota. I am passionate about learning new technologies
                     and experimenting with innovative programming techniques.
                     With my diverse interests and curiosity, I strive to hone my skills through learning, building and
